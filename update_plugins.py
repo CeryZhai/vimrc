26a679c0c81f3c2f7562eca14f472c8cb925c6f6
CeryZhai <zhaixinyi@megvii.com>
2021-09-04 19:37:09 +0800

    add vim-airline

--- update_plugins.py
+++ update_plugins.py
@@ -62,6 +62,8 @@ vim-indent-guides https://github.com/nathanaelkane/vim-indent-guides
 mru.vim https://github.com/vim-scripts/mru.vim
 editorconfig-vim https://github.com/editorconfig/editorconfig-vim
 dracula https://github.com/dracula/vim
+vim-airline https://github.com/vim-airline/vim-airline
+vim-airline-themes https://github.com/vim-airline/vim-airline-themes
 """.strip()
 
 GITHUB_ZIP = "%s/archive/master.zip"
